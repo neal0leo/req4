159fbee5f20e9aee53352e602d7282a0779f070c
neal <neal0leo@163.com>
2019-01-23 19:07:22 +0800

    添加工程到本地仓库

---
+++ Git_Test.java
@@ -0,0 +1,12 @@
+package com.li;
+
+public class Git_Test {
+
+    public static void main(String[] args) {
+        System.out.println("git....");
+        System.out.println("git....");
+        System.out.println("git....");
+        System.out.println("git....");
+        System.out.println("git....");
+    }
+}
